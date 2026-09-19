b34289144d30a7be934c04f5477e684c4f2116c0
Bringways <praveen@techbw.in>
2026-09-19 17:23:36 +0530

    Create ProductRepository interface for product data access

---
+++ repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.bringways.ecommerce.repository;
+
+import com.bringways.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository
+        extends JpaRepository<Product, Long> {
+
+    List<Product> findByCategory(String category);
+
+    List<Product> findByNameContainingIgnoreCase(String name);
+}
